3cb15645d8ccbe231fcfe9c0194231616d2ce3cd
Saqlain-malhi <157057413+Saqlain-malhi@users.noreply.github.com>
2024-08-26 01:27:25 +0500

    Add files via upload

---
+++ index.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a ;
+    int b ;
+    cout<<"Enter the first number:";
+    cin>>a;
+    cout<<"Enter the second number:";
+    cin>>b;
+    cout<<"\n sum of a + b "<< a+b;
+}
